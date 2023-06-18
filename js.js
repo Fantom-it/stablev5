e4f8b5ce0ba687baa5f509161ecf3ffc8aeb265f
Fantom <132481244+Fantom-it@users.noreply.github.com>
2023-06-18 21:46:49 +0300

    Update js.js

--- js.js
+++ js.js
@@ -9,18 +9,30 @@ function settingsMenu() {
     .classList.toggle("show-settings-text-click");
 }
 function stable() {
- document.getElementById("bands-div1").classList.toggle("show-bands");
- document.getElementById("bands-div2").classList.toggle("show-bands");
-  
+  document.getElementById("bands-div1").classList.toggle("show-bands");
+  document.getElementById("bands-div2").classList.toggle("show-bands");
+
   document.getElementById("center-band").classList.toggle("show-center-band");
   document.getElementById("center-band2").classList.toggle("show-center-band2");
 
-  document.getElementById("intermediate-div").classList.toggle("show-intermediate-div");
-}
-function buttonInMain() {
+  document
+    .getElementById("intermediate-div")
+    .classList.toggle("show-intermediate-div");
+  /*document
+    .getElementById("main-page-stable")
+    .classList.toggle("show-main-page-stable-index"); 
+    document
+    .getElementById("main-block-bands")
+    .classList.toggle("show-main-block-bands-index");*/
+
+  /* 1000000000000000000000000000000000000000000000000000000000000000000000000000000 */
   document.getElementById("main-page").classList.toggle("show-main-page");
-  document.getElementById("intermediate-div").classList.toggle("show-intermediate-div-none");
-  document.getElementById("main-page-stable").classList.toggle("show-main-page-stable");
+  document
+    .getElementById("intermediate-div")
+    .classList.toggle("show-intermediate-div-none");
+  document
+    .getElementById("main-page-stable")
+    .classList.toggle("show-main-page-stable");
   document.getElementById("bn-1").classList.toggle("bandsc");
   document.getElementById("bn-2").classList.toggle("bands-2-cl");
   document.getElementById("bn-3").classList.toggle("bands-3-cl");
@@ -61,45 +73,43 @@ function buttonInMain() {
   document.getElementById("bn-md2").classList.toggle("bands-medium-cl");
   document.getElementById("bn-md3").classList.toggle("bands-medium-cl");
   document.getElementById("bn-b-36").classList.toggle("bands-36-cl");
-document.getElementById("bn-b-35").classList.toggle("bands-35-cl");
-document.getElementById("bn-b-34").classList.toggle("bands-34-cl");
- document.getElementById("bn-b-33").classList.toggle("bands-33-cl");
-document.getElementById("bn-b-32").classList.toggle("bands-32-cl");
-document.getElementById("bn-b-31").classList.toggle("bands-31-cl");
+  document.getElementById("bn-b-35").classList.toggle("bands-35-cl");
+  document.getElementById("bn-b-34").classList.toggle("bands-34-cl");
+  document.getElementById("bn-b-33").classList.toggle("bands-33-cl");
+  document.getElementById("bn-b-32").classList.toggle("bands-32-cl");
+  document.getElementById("bn-b-31").classList.toggle("bands-31-cl");
   document.getElementById("bn-b-30").classList.toggle("bands-30-cl");
-   document.getElementById("bn-b-29").classList.toggle("bands-29-cl");
-   document.getElementById("bn-b-28").classList.toggle("bands-28-cl");
-   document.getElementById("bn-b-27").classList.toggle("bands-27-cl");
+  document.getElementById("bn-b-29").classList.toggle("bands-29-cl");
+  document.getElementById("bn-b-28").classList.toggle("bands-28-cl");
+  document.getElementById("bn-b-27").classList.toggle("bands-27-cl");
   document.getElementById("bn-b-26").classList.toggle("bands-26-cl");
-document.getElementById("bn-b-25").classList.toggle("bands-25-cl");
-document.getElementById("bn-b-24").classList.toggle("bands-24-cl");
- document.getElementById("bn-b-23").classList.toggle("bands-23-cl");
-document.getElementById("bn-b-22").classList.toggle("bands-22-cl");
-document.getElementById("bn-b-21").classList.toggle("bands-21-cl");
+  document.getElementById("bn-b-25").classList.toggle("bands-25-cl");
+  document.getElementById("bn-b-24").classList.toggle("bands-24-cl");
+  document.getElementById("bn-b-23").classList.toggle("bands-23-cl");
+  document.getElementById("bn-b-22").classList.toggle("bands-22-cl");
+  document.getElementById("bn-b-21").classList.toggle("bands-21-cl");
   document.getElementById("bn-b-20").classList.toggle("bands-20-cl");
-   document.getElementById("bn-b-19").classList.toggle("bands-19-cl");
+  document.getElementById("bn-b-19").classList.toggle("bands-19-cl");
   document.getElementById("bn-b-18").classList.toggle("bands-18-cl");
   document.getElementById("bn-b-17").classList.toggle("bands-17-cl");
   document.getElementById("bn-b-16").classList.toggle("bands-16-cl");
   document.getElementById("bn-b-15").classList.toggle("bands-15-cl");
   document.getElementById("bn-b-14").classList.toggle("bands-14-cl");
-document.getElementById("bn-b-13").classList.toggle("bands-13-cl");
- document.getElementById("bn-b-12").classList.toggle("bands-12-cl");
+  document.getElementById("bn-b-13").classList.toggle("bands-13-cl");
+  document.getElementById("bn-b-12").classList.toggle("bands-12-cl");
   document.getElementById("bn-b-11").classList.toggle("bands-11-cl");
-document.getElementById("bn-b-10").classList.toggle("bands-10-cl");
-document.getElementById("bn-b-9").classList.toggle("bands-9-cl");
- document.getElementById("bn-b-8").classList.toggle("bands-8-cl");
-document.getElementById("bn-b-7").classList.toggle("bands-7-cl");
+  document.getElementById("bn-b-10").classList.toggle("bands-10-cl");
+  document.getElementById("bn-b-9").classList.toggle("bands-9-cl");
+  document.getElementById("bn-b-8").classList.toggle("bands-8-cl");
+  document.getElementById("bn-b-7").classList.toggle("bands-7-cl");
   document.getElementById("bn-b-6").classList.toggle("bands-6-cl");
-document.getElementById("bn-b-5").classList.toggle("bands-5-cl");
-document.getElementById("bn-b-4").classList.toggle("bands-4-cl");
+  document.getElementById("bn-b-5").classList.toggle("bands-5-cl");
+  document.getElementById("bn-b-4").classList.toggle("bands-4-cl");
   document.getElementById("bn-b-3").classList.toggle("bands-3-cl");
-   document.getElementById("bn-b-2").classList.toggle("bands-2-cl");
+  document.getElementById("bn-b-2").classList.toggle("bands-2-cl");
   document.getElementById("bn-last").classList.toggle("bands-last-cl");
 
-
- 
-document.getElementById("zbn-1").classList.toggle("bandsc-tr");
+  document.getElementById("zbn-1").classList.toggle("bandsc-tr");
   document.getElementById("zbn-2").classList.toggle("bands-2-cl-tr");
   document.getElementById("zbn-3").classList.toggle("bands-3-cl-tr");
   document.getElementById("zbn-4").classList.toggle("bands-4-cl-tr");
@@ -139,48 +149,52 @@ document.getElementById("zbn-1").classList.toggle("bandsc-tr");
   document.getElementById("zbn-md2").classList.toggle("bands-medium-cl-tr");
   document.getElementById("zbn-md3").classList.toggle("bands-medium-cl-tr");
   document.getElementById("zbn-b-36").classList.toggle("bands-36-cl-tr");
-document.getElementById("zbn-b-35").classList.toggle("bands-35-cl-tr");
-document.getElementById("zbn-b-34").classList.toggle("bands-34-cl-tr");
- document.getElementById("zbn-b-33").classList.toggle("bands-33-cl-tr");
-document.getElementById("zbn-b-32").classList.toggle("bands-32-cl-tr");
-document.getElementById("zbn-b-31").classList.toggle("bands-31-cl-tr");
+  document.getElementById("zbn-b-35").classList.toggle("bands-35-cl-tr");
+  document.getElementById("zbn-b-34").classList.toggle("bands-34-cl-tr");
+  document.getElementById("zbn-b-33").classList.toggle("bands-33-cl-tr");
+  document.getElementById("zbn-b-32").classList.toggle("bands-32-cl-tr");
+  document.getElementById("zbn-b-31").classList.toggle("bands-31-cl-tr");
   document.getElementById("zbn-b-30").classList.toggle("bands-30-cl-tr");
-   document.getElementById("zbn-b-29").classList.toggle("bands-29-cl-tr");
-   document.getElementById("zbn-b-28").classList.toggle("bands-28-cl-tr");
-   document.getElementById("zbn-b-27").classList.toggle("bands-27-cl-tr");
+  document.getElementById("zbn-b-29").classList.toggle("bands-29-cl-tr");
+  document.getElementById("zbn-b-28").classList.toggle("bands-28-cl-tr");
+  document.getElementById("zbn-b-27").classList.toggle("bands-27-cl-tr");
   document.getElementById("zbn-b-26").classList.toggle("bands-26-cl-tr");
-document.getElementById("zbn-b-25").classList.toggle("bands-25-cl-tr");
-document.getElementById("zbn-b-24").classList.toggle("bands-24-cl-tr");
- document.getElementById("zbn-b-23").classList.toggle("bands-23-cl-tr");
-document.getElementById("zbn-b-22").classList.toggle("bands-22-cl-tr");
-document.getElementById("zbn-b-21").classList.toggle("bands-21-cl-tr");
+  document.getElementById("zbn-b-25").classList.toggle("bands-25-cl-tr");
+  document.getElementById("zbn-b-24").classList.toggle("bands-24-cl-tr");
+  document.getElementById("zbn-b-23").classList.toggle("bands-23-cl-tr");
+  document.getElementById("zbn-b-22").classList.toggle("bands-22-cl-tr");
+  document.getElementById("zbn-b-21").classList.toggle("bands-21-cl-tr");
   document.getElementById("zbn-b-20").classList.toggle("bands-20-cl-tr");
-   document.getElementById("zbn-b-19").classList.toggle("bands-19-cl-tr");
+  document.getElementById("zbn-b-19").classList.toggle("bands-19-cl-tr");
   document.getElementById("zbn-b-18").classList.toggle("bands-18-cl-tr");
   document.getElementById("zbn-b-17").classList.toggle("bands-17-cl-tr");
   document.getElementById("zbn-b-16").classList.toggle("bands-16-cl-tr");
   document.getElementById("zbn-b-15").classList.toggle("bands-15-cl-tr");
   document.getElementById("zbn-b-14").classList.toggle("bands-14-cl-tr");
-document.getElementById("zbn-b-13").classList.toggle("bands-13-cl-tr");
- document.getElementById("zbn-b-12").classList.toggle("bands-12-cl-tr");
+  document.getElementById("zbn-b-13").classList.toggle("bands-13-cl-tr");
+  document.getElementById("zbn-b-12").classList.toggle("bands-12-cl-tr");
   document.getElementById("zbn-b-11").classList.toggle("bands-11-cl-tr");
-document.getElementById("zbn-b-10").classList.toggle("bands-10-cl-tr");
-document.getElementById("zbn-b-9").classList.toggle("bands-9-cl-tr");
- document.getElementById("zbn-b-8").classList.toggle("bands-8-cl-tr");
-document.getElementById("zbn-b-7").classList.toggle("bands-7-cl-tr");
+  document.getElementById("zbn-b-10").classList.toggle("bands-10-cl-tr");
+  document.getElementById("zbn-b-9").classList.toggle("bands-9-cl-tr");
+  document.getElementById("zbn-b-8").classList.toggle("bands-8-cl-tr");
+  document.getElementById("zbn-b-7").classList.toggle("bands-7-cl-tr");
   document.getElementById("zbn-b-6").classList.toggle("bands-6-cl-tr");
-document.getElementById("zbn-b-5").classList.toggle("bands-5-cl-tr");
-document.getElementById("zbn-b-4").classList.toggle("bands-4-cl-tr");
+  document.getElementById("zbn-b-5").classList.toggle("bands-5-cl-tr");
+  document.getElementById("zbn-b-4").classList.toggle("bands-4-cl-tr");
   document.getElementById("zbn-b-3").classList.toggle("bands-3-cl-tr");
-   document.getElementById("zbn-b-2").classList.toggle("bands-2-cl-tr");
+  document.getElementById("zbn-b-2").classList.toggle("bands-2-cl-tr");
   document.getElementById("zbn-last").classList.toggle("bands-last-cl-tr");
 }
+
+function buttonInMain() {}
 function networkMenu() {
   document.getElementById("network-menu").classList.toggle("show-network-menu");
   document.getElementById("arrow").classList.toggle("show-arrow");
 }
 function networkMenu2() {
-  document.getElementById("network-menu2").classList.toggle("show-network-menu");
+  document
+    .getElementById("network-menu2")
+    .classList.toggle("show-network-menu");
   document.getElementById("arrow2").classList.toggle("show-arrow");
 }
 function tokenMenu() {
